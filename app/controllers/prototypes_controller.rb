0d274f86105cf0beedd115a84539c7799d5b6910
POSOkawa <81221324+POSOkawa@users.noreply.github.com>
2021-04-22 14:57:40 +0900

    修正1457

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   
   def index
     @prototypes = Prototype.includes(:user)
@@ -31,7 +33,7 @@ class PrototypesController < ApplicationController
   def update
     prototype = Prototype.find(params[:id])
     if prototype.update(prototype_params)
-      redirect_to prototype_path
+      redirect_to prototype_path(@prototype)
   else
     render :edit
     end
